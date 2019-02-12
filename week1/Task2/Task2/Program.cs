029ec7e5f6daf1013d30a094f96a451fff3cdb66
tamerlankuankash <qazaqfox@gmail.com>
2019-02-12 10:28:07 +0600

    new tasks

---
+++ week1/Task2/Task2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Student 
+       {
+
+        public string name;
+        public string Id;
+        public int Year;
+    
+        public Student(string name, string Id) // конструктор из двух параметров
+        {
+            this.name = name;
+            this.Id = Id;
+
+        }
+
+          public string NAME() // метод, который будет возращать имя
+        {
+            return name;
+        }
+
+
+        public string ID()  // метод, который будет возращать айдишку студента
+        {
+            return Id;
+
+        }
+        public int YEAR() // метод, который будет возращать год увеличенный 
+        {
+            return ++ Year;
+
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student INFORMATION = new Student(Console.ReadLine(),Console.ReadLine());
+
+            Console.WriteLine(INFORMATION.NAME()); // вызывает функцию NAME, чтобы записать и вывести имя на экран
+            Console.WriteLine(INFORMATION.ID()); // вызывает функцию ID, чтобы записать и вывести айдишку на экран
+            Console.WriteLine(INFORMATION.YEAR()); // вызывает функцию YEAR, чтобы записать и вывести год обучения на экран
+            Console.ReadKey();
+        }
+    }
+}3
---
+++ week1/Task4/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine()); // создаем переменную типа int, куда записываем данные считанные с консольной строки
+            string[,] arr = new string[n, n]; // создаем матрицу с размерностью nxn
+           
+            for (int i = 0; i < n; i++) // создаем цикл, где переменная пробегается от 0 до количества элементов
+            {
+                for (int j = 0; j <= i; j++) // создаем цикл, где новая переменная пробегается от 0 до предыдущей переменной
+                {
+                    arr[i, j] = "[*]"; // элементы матрицы записываются *
+                }
+            }
+
+          
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(arr[i, j]); //!!!!!!!! попробуй это сразу поставить после arr[i, j] = "[*]";
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
